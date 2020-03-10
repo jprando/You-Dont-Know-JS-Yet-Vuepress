c6d426e7b70eb205cab4fe866e72019af149a11d
Jeudi Prando <jeudi@adsoft.com.br>
2020-03-10 20:45:12 -0300

    pwa active

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,16 @@
 module.exports = {
   title: 'You Dont Know JS Yet',
   description: '2nd Edition - book series',
+  head: [
+    ['meta', { httpEquiv: 'Content-Language', content: 'pt-br' }],
+    ['meta', { name: 'language', content: 'Portuguese' }],
+    ['meta', { name: 'generator', content: 'vuepress' }],
+    ['meta', { name: 'no-email-collection', content: 'unspam.com' }],
+    ['meta', { name: 'rating', content: 'general' }],
+    ['meta', { name: 'author', content: 'Jeudi Prando' }],
+    ['meta', { name: 'contact', content: 'jeudiprando@gmail.com' }],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }]
+  ],
   markdown: {
     lineNumbers: true
   },
